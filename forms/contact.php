eafe9a26176bc1bf2e36cb796d7e16aad85d1e2d
Kunal Kohli <kohlim15@gmail.com>
2024-09-15 16:26:21 +0530

    Send email using mailtrap from contact form

---
+++ forms/contact.php
@@ -0,0 +1,33 @@
+<?php
+  $receiving_email_address = 'kohlim15@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  
+  $contact->smtp = array(
+    'protocol' => 'smtp',
+    'smtp_host' => 'sandbox.smtp.mailtrap.io',
+    'smtp_port' => 2525,
+    'smtp_user' => 'a13ee367c613c5',
+    'smtp_pass' => '5b7f63ead206eb',
+  );
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
